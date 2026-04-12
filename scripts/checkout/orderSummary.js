2661c94bef8f026788a2558714eae52772fcae57
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2026-04-12 22:21:11 +0100

    update order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { cart, removeItem, updateQuantity, calculateCartQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct} from "../../data/products.js";
 import { formatMoney } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryoption.js"; 
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryoption.js"; 
 
 /*   hello();
       const today = dayjs();
@@ -15,23 +15,11 @@ export function renderOrderSummary() {
   let cartSummaryHTML= '';
   cart.forEach((cartItem)=>{
       const productId = cartItem.productId;
-
-      let matchingProduct;
-
-      products.forEach((product)=>{
-          if (product.id=== productId){
-              matchingProduct= product;
-          }
-      })
+      const matchingProduct = getProduct(productId);
 
       const deliveryOptionId = cartItem.deliveryOptionId;
 
-      let deliveryOption;
-        deliveryOptions.forEach((option) => {
-          if (option.id === deliveryOptionId) {
-            deliveryOption = option;
-          }
-        });
+      const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
         const deliveryDate = today.add(
