49f0f87916d0be948b6dce0fef65e609fc1f88dd
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2026-04-18 03:09:02 +0100

    create automated tests

---
+++ tests/moneyTest.js
@@ -0,0 +1,26 @@
+import { formatMoney } from "../scripts/utils/money.js";
+
+console.log("test suite: formatMoney");
+
+if (formatMoney(2095) === "20.95") {
+    console.log("Passed");
+}else {
+    console.log("failed");
+}
+
+if (formatMoney(0) === "0.00") {
+    console.log ("passed");
+}else {
+    console.log('failed');
+}
+
+if (formatMoney(2000.5) === '20.01') {
+    console.log ("Passed")
+}else {
+    console.log("failed")
+}
+if (formatMoney(2000.4) === "20.00") {
+    console.log("Passed")
+}else {
+    console.log("failed");
+}
