7b1568b6b5be1ed1e0c045d22ff64d5981d77fa0
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2026-05-01 15:44:36 +0100

    tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -8,12 +8,14 @@ if (formatMoney(2095) === "20.95") {
     console.log("failed");
 }
 
+console.log ("works with zero");
 if (formatMoney(0) === "0.00") {
     console.log ("passed");
 }else {
     console.log('failed');
 }
 
+console.log("rounds up to the nearest cent");
 if (formatMoney(2000.5) === '20.01') {
     console.log ("Passed")
 }else {
