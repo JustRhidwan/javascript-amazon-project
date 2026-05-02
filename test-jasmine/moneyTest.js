38f04fe46c1962888371722bc41dd2eba73df7cd
JustRhidwan <107209973+JustRhidwan@users.noreply.github.com>
2026-05-02 08:29:45 +0100

    remove defualt test

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { formatMoney } from "../scripts/utils/money.js";
+
+describe("test suite: formatMoney", () => {
+    it("converts cents to dollars", () => {
+        expect(formatMoney(2095)).toEqual('20.95');
+    });
+    it("works with zero", () => {
+        expect(formatMoney(0)).toEqual('0.00');
+    });
+    it("rounds up to the nearest cent", () => {
+        expect(formatMoney(2000.5)).toEqual("20.01");
+    });
+});
